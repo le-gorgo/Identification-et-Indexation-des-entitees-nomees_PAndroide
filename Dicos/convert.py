250a7dff0afdcdc1aca097275c9389c961e21dfc
Julien <julien.willaime@gmail.com>
2021-04-19 00:11:19 +0200

    rangement fichiers+ configuration avec un fichier+ nom les + long en 1er

--- Dicos/convert.py
+++ Dicos/convert.py
@@ -1,6 +1,6 @@
 import codecs
 dicos=["dicoTest.txt","testLieux.txt","testAuthors.txt","testOeuvres.txt","testPers.txt","testInst.txt","testCrit.txt"]
-misc=["testNoise0.txt","testCommun0.txt"]
+misc=["testNoise.txt","testCommun.txt"]
 id=0
 for dico in dicos:
 
@@ -14,10 +14,11 @@ for dico in dicos:
         id+=1
 
 for dico in misc:
-        t=codecs.open(dico, "r", "utf-8", "replace")
-        lines=t.read().splitlines()
-        t.close()
-        f = codecs.open("converted_"+dico, "w", "utf-8", "replace")
-        res=""
-        for line in lines:
-            f.write(line+":\n")
+
+    t=codecs.open(dico, "r", "utf-8", "replace")
+    lines=t.read().splitlines()
+    t.close()
+    f = codecs.open("converted_"+dico, "w", "utf-8", "replace")
+    res=""
+    for line in lines:
+        f.write(line+":\n")
--- testload.py
+++
@@ -1,3 +0,0 @@
-import filter3
-
-print(filter3.loadRef("converted_testAuthors.txt",0))
